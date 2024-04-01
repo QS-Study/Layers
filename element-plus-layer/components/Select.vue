d84ba6da616d4c38f57aac2ab86483cc6d8ad409
Lami <Halam.Lee.Lami@gmail.com>
2024-04-01 13:32:57 +0900

    feat: element-plus-layer 생성

---
+++ element-plus-layer/components/Select.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const value = ref('');
+
+const options = [
+  {
+    value: 'element plus',
+    label: 'element plus',
+  },
+  {
+    value: 'test',
+    label: 'test',
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+];
+</script>
+
+<template>
+  <div>
+    <el-select
+      v-model="value"
+      placeholder="Select"
+      size="large"
+      style="width: 240px">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value" />
+    </el-select>
+  </div>
+</template>
