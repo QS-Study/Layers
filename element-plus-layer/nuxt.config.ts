fe1beb7879ade96a8353fea16ea657d8c2e25507
Lami <Halam.Lee.Lami@gmail.com>
2024-04-02 17:34:24 +0900

    release: v1.0.5 - element-plus css 관련 설정 추가

--- element-plus-layer/nuxt.config.ts
+++ element-plus-layer/nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   modules: ['@pinia/nuxt', '@element-plus/nuxt'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/scss/element/index.scss" as element;`,
+        },
+      },
+    },
+  },
 });
