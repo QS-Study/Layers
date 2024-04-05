48dcdcdca8b8f8a55f93361ff5e4dbe4b2504917
Lami <Halam.Lee.Lami@gmail.com>
2024-04-05 15:18:40 +0900

    release: v1.0.13 - element-plus plugin, gitattributes 추가

--- element-plus-layer/nuxt.config.ts
+++ element-plus-layer/nuxt.config.ts
@@ -1,11 +1,5 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const currentDir = dirname(fileURLToPath(import.meta.url));
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   ssr: false,
   modules: ['@pinia/nuxt', '@element-plus/nuxt'],
   vite: {
@@ -13,9 +7,4 @@ export default defineNuxtConfig({
       include: ['dayjs', 'dayjs/plugin/*'],
     },
   },
-  // elementPlus: {
-  //   importStyle: 'scss',
-  // },
-  // css
-  // css: [join(currentDir, './assets/scss/index.scss')],
 });
